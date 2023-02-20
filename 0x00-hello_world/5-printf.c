7670a8eb5a5be71a8f75f481866beae55a451d20
maggysipoi <cpoimaggy@gmail.com>
2023-02-19 18:52:54 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#include<stdio.h)
+#include<stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Description: A C program that prints with printf function.
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
