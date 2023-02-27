563043a5e3a3ff30ece70833d6cb7c652cb09dc0
maggysipoi <cpoimaggy@gmail.com>
2023-02-27 02:00:55 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,16 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * _isupper - check the code for Holberton School students.
- *@c : variable
- * Return: 0 or 1 .
- */
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+*/
+
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
-else
-{
-return (0);
-}
-return (0);
-}	
+	
